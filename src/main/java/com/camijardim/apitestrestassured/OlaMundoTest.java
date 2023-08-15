3f1d815fd21fd52f37c2959a05496eefb552f603
Camila Francisco <camila.francisco.external@lojasrenner.com.br>
2023-08-15 11:23:40 -0300

    inserindo MatcherMancrest iniciais

--- src/main/java/com/camijardim/apitestrestassured/OlaMundoTest.java
+++ src/main/java/com/camijardim/apitestrestassured/OlaMundoTest.java
@@ -4,10 +4,16 @@ import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
 
 public class OlaMundoTest {
 
@@ -42,6 +48,30 @@ public class OlaMundoTest {
             .get("https://restapi.wcaquino.me/ola")
         .then()
             .statusCode(200);
+    }
+
+    @Test
+    public void devoConhecerMatcherHamcrest() {
+        assertThat("Maria", Matchers.is("Maria"));
+        assertThat(128, Matchers.is(128));
+        assertThat(128, Matchers.isA(Integer.class));
+        assertThat(128d, Matchers.isA(Double.class));
+        assertThat(128d, Matchers.greaterThan(120d));
+        assertThat(128d, Matchers.lessThan(130d));
+
+        List<Integer> impares = Arrays.asList(1,3,5,7,9);
+        //Assert.assertThat(impares, Matchers.<Integer>hasSize(5)); - SEM IMPORT STATIC
+        assertThat(impares, hasSize(5));
+        assertThat(impares, contains(1,3,5,7,9)); // tem q ser na msm ordem
+        assertThat(impares, containsInAnyOrder(1,7,9,3,5)); //conferir se tem os elementos msm passando fora da ordem
+        assertThat(impares, hasItem(1)); //se contém APENAS UM elemento mesmo n passando todos os elementos presentes
+        assertThat(impares, hasItems(1, 5));
+
+        assertThat("Maria", is(not("João")));
+        assertThat("Maria", not("João"));
+        assertThat("Joaquina", anyOf(is("Maria"), is("Joaquina")));
+        assertThat("Joaquina", allOf(startsWith("Joa"), endsWith("ina"), containsString("qui")));
+
     }
 
 }
